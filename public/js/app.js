cb757af6fca860dbf225064e40eafd9b73b1d5b4
hyxud <hyxud.dev@gmail.com>
2022-10-19 22:34:17 +0100

    Add topbar functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,12 @@
-let sidebarButton = document.getElementById("sidebar-button")
-let sidebar = document.getElementById("sidebar")
-let closeNMButton = document.querySelector("#sc-warn > button")
+var sidebarButton = document.getElementById("sidebar-button")
+var sidebar = document.getElementById("sidebar")
+var topbar = document.getElementById("topbar")
+var content = document.getElementById("content")
+var closeNMButton = document.querySelector("#sc-warn > button")
 
-let isSidebarOpened = false
+var isSidebarOpened = false
+
+
 sidebarButton.addEventListener('click', ()=>{
     sidebar.style.left = isSidebarOpened ? "-190px" : "0px"
     isSidebarOpened = !isSidebarOpened
@@ -12,6 +16,25 @@ closeNMButton.addEventListener('click', ()=>{
     document.querySelector("#sc-warn").remove()
     document.querySelector("#sc-warn-bg").remove()
 })
+
+
+
+function setupTopbar() {
+    if (isMobile) {
+        topbar.remove()
+        return    
+    }
+    let sections = content.children
+    for (let i = 0; i < sections.length; i++) {
+        const section = sections[i];
+        let newElem = document.createElement("div")
+        newElem.innerHTML = "<div>"+section.getAttribute("name")+"</div>"
+        
+        topbar.appendChild(newElem)
+    }
+}
+
+setupTopbar()
 
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
