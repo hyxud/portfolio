f512e7c2f2ab4d16a8588b2f42688721969fd8c6
hyxud <hyxud.dev@gmail.com>
2022-10-20 12:52:06 +0100

    auto deploy edit

--- deploy.js
+++ deploy.js
@@ -1,11 +1,12 @@
 const { exec } = require("child_process");
 const { spawn } = require("child_process");
+const FIREBASE = `\"${process.env.npm_config_global_prefix}\\firebase\"`
 
 
 
 function deploy(branch) {
     let acmd = []
-    let cmd = "firebase"
+    let cmd = FIREBASE
     if (branch=="main"){
         acmd = ["deploy", "--only", "hosting:hyxud-server"] 
     }
@@ -16,30 +17,23 @@ function deploy(branch) {
         console.log(`Unknown Branch: ${branch}\nCanceled Deploy.`);
         return
     }
-    let deploycmd = spawn("firebase", acmd);
     acmd.forEach(param => {
         cmd = `${cmd} ${param}`
     });
     console.log(cmd);
-    deploycmd.stdout.on("data", data => {
-        console.log(`stdout: ${data}`);
-    });
-
-    deploycmd.stderr.on("data", data => {
-        console.log(`stderr: ${data}`);
-    });
-
-    deploycmd.on('error', (error) => {
-        console.log(`error: ${error.message}`);
-    });
-
-    deploycmd.on("close", code => {
-        console.log(`child process exited with code ${code}`);
-    });
+    console.log(acmd);
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(stdout);
+    })
 }
-
-
-
 exec("git rev-parse --abbrev-ref HEAD", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
@@ -51,5 +45,25 @@ exec("git rev-parse --abbrev-ref HEAD", (error, stdout, stderr) => {
     }
     stdout = stdout.slice(0, -1);
     console.log(`Current Branch: ${stdout}`);
+    if (process.argv[2] == "t") {
+        let spawncmd = spawn(FIREBASE, ["login"]);
+        spawncmd.stdout.on("data", data => {
+            console.log(`stdout: ${data}`);
+        });
+
+        spawncmd.stderr.on("data", data => {
+            console.log(`stderr: ${data}`);
+        });
+
+        spawncmd.on('error', (error) => {
+            console.log(`error: ${error.message}`);
+        });
+
+        spawncmd.on("close", code => {
+            console.log(`child process exited with code ${code}`);
+        });
+        return
+    }
+    
     deploy(stdout)
 });
