08ffecd4b964dca23c9567d4fa8bd20439900827
hyxud <hyxud.dev@gmail.com>
2022-10-20 20:10:04 +0100

    Modified 404.html

--- public/js/branchSpecific.js
+++ public/js/branchSpecific.js
@@ -2,5 +2,6 @@ console.log("DEV BUILD");
 
 const wipText = document.getElementById("wip")
 const backButtonLink = document.getElementById("back-btn-a")
+
 wipText.innerHTML = "DEV BUILD"
 backButtonLink.setAttribute("href", "hyxud-devserver.web.app")
