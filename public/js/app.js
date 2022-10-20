6ea564284eb5060ad87958baec4cb099f91cfbbf
hyxud <hyxud.dev@gmail.com>
2022-10-20 22:18:38 +0100

    Removed Unfinished Toolbar Feature

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,5 @@
 var sidebarButton = document.getElementById("sidebar-button")
 var sidebar = document.getElementById("sidebar")
-var topbar = document.getElementById("topbar")
 var content = document.getElementById("content")
 var closeNMButton = document.querySelector("#sc-warn > button")
 
@@ -16,25 +15,6 @@ closeNMButton.addEventListener('click', ()=>{
     document.querySelector("#sc-warn").remove()
     document.querySelector("#sc-warn-bg").remove()
 })
-
-
-
-function setupTopbar() {
-    if (isMobile) {
-        topbar.remove()
-        return    
-    }
-    let sections = content.children
-    for (let i = 0; i < sections.length; i++) {
-        const section = sections[i];
-        let newElem = document.createElement("div")
-        newElem.innerHTML = "<div>"+section.getAttribute("name")+"</div>"
-        
-        topbar.appendChild(newElem)
-    }
-}
-
-setupTopbar()
 
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
