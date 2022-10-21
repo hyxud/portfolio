741d76d2f08ee1a455a5281684380866b2ff4c18
hyxud <hyxud.dev@gmail.com>
2022-10-21 21:42:33 +0100

    Minor Changes

--- public/js/BSindex.js
+++ public/js/BSindex.js
@@ -1,6 +1,7 @@
 console.log("DEV VERSION");
 
+const version = 44
 const wipText = document.getElementById("wip")
 
 
-wipText.innerHTML = "DEVELOPMENT VERSION"
+wipText.innerHTML = "DEVELOPMENT VERSION: "+version
