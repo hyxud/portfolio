a1ea891c846081474d14cdad327a68f61c8b89b7
hyxud <hyxud.dev@gmail.com>
2022-10-27 00:20:51 +0100

    more parallax

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,6 +3,8 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import About from "./About";
 import './css/content.css'
 import backgroundImage from "../images/bg.jpg";
+import backgroundImageF from "../images/bgf.png";
+import backgroundImageB from "../images/bgb.png";
 
 function Content() {
   
@@ -14,14 +16,26 @@ function Content() {
           speed={.2}
           factor={1}
           style={{
-            backgroundImage: `url(${backgroundImage})`,
+            backgroundImage: `url(${backgroundImageB})`,
             backgroundRepeat: 'no-repeat',
             backgroundPosition: 'center',
             backgroundSize: 'cover',
           }}></ParallaxLayer>
-          
+        <ParallaxLayer
+          offset={0}
+          speed={.5}
+          factor={1}
+          style={{
+            backgroundImage: `url(${backgroundImageF})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+          }}></ParallaxLayer>
         <ParallaxLayer factor={1} speed={1}>
           <About />
+        </ParallaxLayer>
+        <ParallaxLayer factor={.1} offset={.99} speed={.5} style={{background: 'rgb(18, 22, 34)'}}>
+          
         </ParallaxLayer>
       </Parallax>
     </div>
