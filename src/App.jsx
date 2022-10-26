c76f1051e0faff6f87d985f60aa24319d9e9e8cd
hyxud <hyxud.dev@gmail.com>
2022-10-26 22:55:35 +0100

    Added Loading screen fade out

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Navbar from "./components/Navbar";
 import Content from "./components/Content";
 import "./components/css/App.css";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
-  useEffect(() => {
+  const [loadingVisible, setLoadingVisible] = useState(true)
+  const loadingDiv = useRef(null)
+  var loadStyles = {
+    display: loadingVisible ? 'flex' : 'none'
+  }
+  const onPageLoad = () => {
     setLoaded(true);
-  }, []);
+    loadingDiv.current.classList.add("load-anim")
+    setTimeout(()=>{
+      setLoadingVisible(false)
+    }, 1000)
+  }
 
-  let loadStyle = {
-    display: loaded ? "none" : "flex",
-  };
+  useEffect(() => {
+    if (document.readyState === 'complete') {
+      onPageLoad();
+    } else {
+      window.addEventListener('load', onPageLoad);
+      return () => window.removeEventListener('load', onPageLoad);
+    }
+  }, []);
 
   return (
     <div className="App">
-      <div id="loading-div" style={loadStyle}>
+      <div id="loading-div" ref={loadingDiv} style={loadStyles}>
         <h1 class="loading-div-text">Loading</h1>
       </div>
       <Navbar/>
